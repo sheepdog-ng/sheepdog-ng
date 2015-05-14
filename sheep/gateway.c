2104553d7215b0a476562a3e2bdb69beb0527eeb
Liu Yuan <liuyuan@cmss.chinamobile.com>
2015-05-14 15:03:01 +0800

    Revert "sheep: update inode coherence protocol for iSCSI multipath"
    This reverts commit 8cdef5be8b421c74d02be9812a24c248cc33a7ea.
    
    Conflicts:
    	sheep/gateway.c

--- sheep/gateway.c
+++ sheep/gateway.c
@@ -615,32 +615,12 @@ static int update_obj_refcnt(const struct sd_req *hdr, uint32_t *vids,
 			       false);
 }
 
-static bool is_inode_refresh_req(struct request *req)
-{
-	uint64_t oid = req->rq.obj.oid;
-	uint32_t data_length = req->rq.data_length;
-	uint32_t offset = req->rq.obj.offset;
-
-	if (!is_vdi_obj(oid))
-		return false;
-
-	if (offset)
-		return false;
-
-	if (data_length != data_vid_offset(SD_INODE_DATA_INDEX))
-		return false;
-
-	return true;
-}
-
 int gateway_read_obj(struct request *req)
 {
 	uint64_t oid = req->rq.obj.oid;
 	int ret;
 
-	if ((req->rq.flags & SD_FLAG_CMD_TGT) &&
-	    !is_inode_refresh_req(req) &&
-	    is_refresh_required(oid_to_vid(oid))) {
+	if (!is_vdi_obj(oid) && is_refresh_required(oid_to_vid(oid))) {
 		sd_debug("refresh is required: %"PRIx64, oid);
 		return SD_RES_INODE_INVALIDATED;
 	}
@@ -657,9 +637,7 @@ int gateway_read_obj(struct request *req)
 	if (ret != SD_RES_SUCCESS)
 		return ret;
 
-	if (is_inode_refresh_req(req))
-		validate_myself(oid_to_vid(oid));
-
+	validate_myself(oid_to_vid(oid));
 	return ret;
 }
 
@@ -671,8 +649,7 @@ int gateway_write_obj(struct request *req)
 	uint32_t *vids = NULL, *new_vids = req->data;
 	struct generation_reference *refs = NULL;
 
-	if ((req->rq.flags & SD_FLAG_CMD_TGT) &&
-	    is_refresh_required(oid_to_vid(oid))) {
+	if (is_refresh_required(oid_to_vid(oid))) {
 		sd_debug("refresh is required: %"PRIx64, oid);
 		return SD_RES_INODE_INVALIDATED;
 	}
@@ -706,7 +683,6 @@ int gateway_write_obj(struct request *req)
 		update_obj_refcnt(hdr, vids, new_vids, refs);
 	}
 out:
-
 	free(vids);
 	free(refs);
 	return ret;
@@ -746,12 +722,6 @@ out:
 int gateway_create_and_write_obj(struct request *req)
 {
 	uint64_t oid = req->rq.obj.oid;
-
-	if ((req->rq.flags & SD_FLAG_CMD_TGT) &&
-	    is_refresh_required(oid_to_vid(oid))) {
-		sd_debug("refresh is required: %"PRIx64, oid);
-		return SD_RES_INODE_INVALIDATED;
-	}
 
 	if (oid_is_readonly(oid))
 		return SD_RES_READONLY;
