a7e53d147f76e59b00ddb0529ee397493a73921e
Yu Yang <yuyang@cmss.chinamobile.com>
2015-06-05 17:08:58 +0800

    shared lib: add VDI operation: snapshot
    Take a snapshot of a working VDI.
    We do NOT import snapshot id for lib user, snapshot tag is
    necessary for any snapshot VDI.
    
    Signed-off-by: Yu Yang <yuyang@cmss.chinamobile.com>
    Signed-off-by: Liu Yuan <liuyuan@cmss.chinamobile.com>

--- lib/shared/vdi.c
+++ lib/shared/vdi.c
@@ -14,6 +14,19 @@
 #include "sheepdog.h"
 #include "internal.h"
 
+static int write_object(struct sd_cluster *c, uint64_t oid, uint64_t cow_oid,
+	void *data, unsigned int datalen, uint64_t offset, uint32_t flags,
+	bool create, bool direct);
+
+static int read_object(struct sd_cluster *c, uint64_t oid, void *data,
+		unsigned int datalen, uint64_t offset, bool direct);
+
+static int vdi_read_inode(struct sd_cluster *c, char *name,
+	char *tag, struct sd_inode *inode, bool onlyheader);
+
+static int find_vdi(struct sd_cluster *c, char *name,
+					char *tag, uint32_t *vid);
+
 static int lock_vdi(struct sd_vdi *vdi)
 {
 	struct sd_req hdr = {};
@@ -187,3 +200,197 @@ int sd_vdi_close(struct sd_vdi *vdi)
 	free_vdi(vdi);
 	return 0;
 }
+
+static int do_vdi_create(struct sd_cluster *c, char *name, uint64_t vdi_size,
+			uint32_t base_vid, uint32_t *vdi_id,
+			bool snapshot, uint8_t store_policy)
+{
+	struct sd_req hdr = {};
+	struct sd_rsp *rsp = (struct sd_rsp *)&hdr;
+	int ret;
+
+	hdr.opcode = SD_OP_NEW_VDI;
+	hdr.flags = SD_FLAG_CMD_WRITE;
+	hdr.data_length = SD_MAX_VDI_LEN;
+
+	hdr.vdi.base_vdi_id = base_vid;
+	hdr.vdi.snapid = snapshot ? 1 : 0;
+	hdr.vdi.vdi_size = vdi_size;
+	hdr.vdi.store_policy = store_policy;
+
+	ret = sd_run_sdreq(c, &hdr, name);
+	if (ret != SD_RES_SUCCESS)
+		return ret;
+
+	if (vdi_id)
+		*vdi_id = rsp->vdi.vdi_id;
+
+	return SD_RES_SUCCESS;
+}
+
+static int write_object(struct sd_cluster *c, uint64_t oid, uint64_t cow_oid,
+	void *data, unsigned int datalen, uint64_t offset, uint32_t flags,
+	bool create, bool direct)
+{
+	struct sd_req hdr = {};
+	struct sd_rsp *rsp = (struct sd_rsp *)&hdr;
+	int ret;
+
+	if (create)
+		hdr.opcode = SD_OP_CREATE_AND_WRITE_OBJ;
+	else
+		hdr.opcode = SD_OP_WRITE_OBJ;
+	hdr.data_length = datalen;
+	hdr.flags = flags | SD_FLAG_CMD_WRITE;
+	if (cow_oid)
+		hdr.flags |= SD_FLAG_CMD_COW;
+	if (direct)
+		hdr.flags |= SD_FLAG_CMD_DIRECT;
+
+	hdr.obj.oid = oid;
+	hdr.obj.cow_oid = cow_oid;
+	hdr.obj.offset = offset;
+
+	ret = sd_run_sdreq(c, &hdr, data);
+	if (ret != SD_RES_SUCCESS)
+		return ret;
+
+	if (rsp->result != SD_RES_SUCCESS)
+		return rsp->result;
+
+	return SD_RES_SUCCESS;
+}
+
+static int read_object(struct sd_cluster *c, uint64_t oid, void *data,
+		unsigned int datalen, uint64_t offset, bool direct)
+{
+	struct sd_req hdr = {};
+	struct sd_rsp *rsp = (struct sd_rsp *)&hdr;
+	int ret;
+
+	hdr.opcode = SD_OP_READ_OBJ;
+	hdr.data_length = datalen;
+	hdr.obj.oid = oid;
+	hdr.obj.offset = offset;
+	if (direct)
+		hdr.flags |= SD_FLAG_CMD_DIRECT;
+
+	ret = sd_run_sdreq(c, &hdr, data);
+	if (ret != SD_RES_SUCCESS)
+		return ret;
+
+	if (rsp->result != SD_RES_SUCCESS)
+		return rsp->result;
+
+	return SD_RES_SUCCESS;
+}
+
+static int find_vdi(struct sd_cluster *c, char *name,
+					char *tag, uint32_t *vid)
+{
+	struct sd_req hdr = {};
+	struct sd_rsp *rsp = (struct sd_rsp *)&hdr;
+	char buf[SD_MAX_VDI_LEN + SD_MAX_VDI_TAG_LEN];
+	int ret;
+
+	memset(buf, 0, sizeof(buf));
+	pstrcpy(buf, SD_MAX_VDI_LEN, name);
+	if (tag)
+		pstrcpy(buf + SD_MAX_VDI_LEN, SD_MAX_VDI_TAG_LEN, tag);
+
+	hdr.opcode = SD_OP_GET_VDI_INFO;
+	hdr.data_length = SD_MAX_VDI_LEN + SD_MAX_VDI_TAG_LEN;
+	hdr.flags = SD_FLAG_CMD_WRITE;
+
+	ret = sd_run_sdreq(c, &hdr, buf);
+	if (ret != SD_RES_SUCCESS)
+		return ret;
+
+	if (rsp->result == SD_RES_SUCCESS && vid)
+		*vid = rsp->vdi.vdi_id;
+
+	return rsp->result;
+}
+
+static int vdi_read_inode(struct sd_cluster *c, char *name,
+	char *tag, struct sd_inode *inode, bool onlyheader)
+{
+	int ret;
+	uint32_t vid;
+	size_t len;
+
+	ret = find_vdi(c, name, tag, &vid);
+	if (ret != SD_RES_SUCCESS)
+		return ret;
+
+	if (onlyheader)
+		len = SD_INODE_HEADER_SIZE;
+	else
+		len = SD_INODE_SIZE;
+
+	ret = read_object(c, vid_to_vdi_oid(vid), inode, len, 0, true);
+
+	return ret;
+}
+
+int sd_vdi_snapshot(struct sd_cluster *c, char *name, char *snap_tag)
+{
+	char buf[SD_INODE_HEADER_SIZE];
+	struct sd_inode *inode = (struct sd_inode *)buf;
+	int ret = 0;
+
+	if (!snap_tag || *snap_tag == '\0') {
+		fprintf(stderr, "Snapshot tag can NOT be null!\n");
+		return SD_RES_INVALID_PARMS;
+	}
+
+	if (!name || *name == '\0') {
+		fprintf(stderr, "VDI name can NOT be null!\n");
+		return SD_RES_INVALID_PARMS;
+	}
+
+	ret = find_vdi(c, name, snap_tag, NULL);
+
+	if (ret == SD_RES_SUCCESS) {
+		fprintf(stderr, "Failed to create snapshot: "
+			"Tag %s is already existed\n", snap_tag);
+		return SD_RES_INVALID_PARMS;
+
+	} else if (ret == SD_RES_NO_TAG) {
+		ret = vdi_read_inode(c, name, NULL, inode, true);
+		if (ret != SD_RES_SUCCESS) {
+			fprintf(stderr, "Failed to read inode: %s\n", name);
+			return ret;
+		}
+
+	} else {
+		fprintf(stderr, "Failed to create snapshot:%s\n",
+				sd_strerr(ret));
+		return ret;
+	}
+
+	if (inode->store_policy) {
+		fprintf(stderr, "Creating a snapshot of hypervolume"
+				" is not supported\n");
+		return SD_RES_INVALID_PARMS;
+	}
+
+	ret = write_object(c, vid_to_vdi_oid(inode->vdi_id), 0, snap_tag,
+			SD_MAX_VDI_TAG_LEN, offsetof(struct sd_inode, tag), 0,
+			false, false);
+
+	if (ret != SD_RES_SUCCESS) {
+		fprintf(stderr, "Failed to write object:%"PRIx64"\n",
+				vid_to_vdi_oid(inode->vdi_id));
+		return ret;
+	}
+
+	ret = do_vdi_create(c, inode->name, inode->vdi_size,
+			inode->vdi_id, NULL, true, 0);
+
+	if (ret != SD_RES_SUCCESS)
+		fprintf(stderr, "Failed to snapshot:%s\n", sd_strerr(ret));
+
+	return ret;
+}
+
